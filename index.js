d57e40698cd52117598ffc56b3d2536a4b114047
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-07-31 22:36:48 +0530

    Learning hoesting

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log(a);
-//hoiesting is a java script default behaviours where all the declaration move to the top of their scope
+//hoiesting  is a java script default behaviours where all the declaration move to the top of their scope
